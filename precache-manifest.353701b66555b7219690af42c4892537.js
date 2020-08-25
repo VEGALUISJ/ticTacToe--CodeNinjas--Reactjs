b1a737f278a48ffa0d58dece9ee82cb2c633ad3b
Jose Vega <luisvega.jlvv@gmail.com>
2020-08-25 12:36:05 -0400

    Updates

---
+++ precache-manifest.353701b66555b7219690af42c4892537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25d4b8e5fcaf0271d3ead1e63e5cf08",
+    "url": "/ticTacToe-CodeNinjas-Reactjs.github.io/index.html"
+  },
+  {
+    "revision": "8455ae0f9d869e240052",
+    "url": "/ticTacToe-CodeNinjas-Reactjs.github.io/static/css/main.c5482488.chunk.css"
+  },
+  {
+    "revision": "6621ea4868e14185b319",
+    "url": "/ticTacToe-CodeNinjas-Reactjs.github.io/static/js/2.5ade933b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticTacToe-CodeNinjas-Reactjs.github.io/static/js/2.5ade933b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8455ae0f9d869e240052",
+    "url": "/ticTacToe-CodeNinjas-Reactjs.github.io/static/js/main.9d1c9c5a.chunk.js"
+  },
+  {
+    "revision": "521233fcccd533ec12bc",
+    "url": "/ticTacToe-CodeNinjas-Reactjs.github.io/static/js/runtime-main.8508be33.js"
+  }
+]);
